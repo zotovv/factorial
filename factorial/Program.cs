c2cb76f985dd75be530a760b3bc8fb034bd8fb23
Vladimir Zotov <zott14@outlook.com>
2018-03-27 14:45:15 +0300

    Program id created.

--- factorial/Program.cs
+++ factorial/Program.cs
@@ -10,6 +10,20 @@ namespace factorial
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите количество клиентов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+
+            do
+            {
+                factorial *= n--;
+            }
+            while (n > 0);
+
+            Console.WriteLine("Всего вариантов доставки: {0}", factorial);
+
+            Console.ReadKey();
+                
         }
     }
 }
